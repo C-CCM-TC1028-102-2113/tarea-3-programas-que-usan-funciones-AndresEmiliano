58f301dd1525c4b6eec5b738614534013604e048
AndresEmiliano <89090646+AndresEmiliano@users.noreply.github.com>
2021-08-30 23:58:48 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,7 +1,20 @@
 
 def main():
     #escribe tu código abajo de esta línea
-    pass
+def bisiesto(h):
+    if h%4==0:
+        if h%100 !=0 or h%400==0:
+            print("true")
+        else:
+            print("false")
+    else:
+        print("false")
+
+def main():
+    h = int(input("dame el año:"))
+    a = bisiesto(h)
+
+main()
 
 if __name__=='__main__':
     main()
